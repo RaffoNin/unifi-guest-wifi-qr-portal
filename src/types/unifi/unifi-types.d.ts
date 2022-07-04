55ae32cb19954d942d4250cbe27a6928d90ec390
Ervin Ligahon <ervinligahon@gmail.com>
2022-07-05 01:01:47 +0800

    add enabled type

--- src/types/unifi/unifi-types.d.ts
+++ src/types/unifi/unifi-types.d.ts
@@ -3,6 +3,7 @@ export type wifiSecurityProtocol = 'WPA' | 'WEP' | 'none';
 export interface IWlanSettings {
     _id: string;
     name: string;
+    enabled: boolean;
     x_passphrase: string;
     security: string;
     hide_ssid: boolean;
