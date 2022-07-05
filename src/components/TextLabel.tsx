854633148f38c5a878c88ec42e202de97605f97f
Ervin Ligahon <ervinligahon@gmail.com>
2022-07-05 21:13:25 +0800

    add props to div props to component

--- src/components/TextLabel.tsx
+++ src/components/TextLabel.tsx
@@ -1,15 +1,27 @@
 import React, {LegacyRef, useRef} from 'react';
 
-interface TextLabelProps {
+interface TextLabelProps
+    extends React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLDivElement>,
+        HTMLDivElement
+    > {
     title: string;
     body: string;
 }
 
-const TextLabel: React.FC<TextLabelProps> = ({title, body}) => {
+const TextLabel: React.FC<TextLabelProps> = ({
+    title,
+    body,
+    className,
+    ...props
+}) => {
     const bodyRef: LegacyRef<HTMLHeadingElement> | undefined = useRef(null);
 
     return (
-        <div className="flex w-full gap-x-1 justify-center flex-wrap">
+        <div
+            className={`flex w-full gap-x-1 justify-center flex-wrap ${className}`}
+            {...props}
+        >
             <h3 className="">{title + ':'}</h3>
             <h4
                 ref={bodyRef}
